ac29e5fc47d61ebec61717918f5804b20ebc64ac
donMaleesha <lochanamt@gmail.com>
2024-11-22 17:53:54 +0530

    ViewRelaxationAcitivities.php

--- App/controller/ViewRelaxationActivityController.php
+++ App/controller/ViewRelaxationActivityController.php
@@ -1,68 +1,51 @@
 <?php
 
-    require_once "../models/RelaxationActivityModel.php";
+require_once "../models/ViewRelaxationActivityModel.php";
 
-    class ViewRelaxationActivityController {
-        private $model;
+class ViewRelaxationActivityController {
 
-        public function __construct() {
-            $this->model = new RelaxationActivityModel();
-        }
+    private $model;
 
-        public function handleRequest() {
-            require_once '../../config/config.php';
-      $db = new Database();
-      $conn = $db->connect();
-      
-      $sql = "SELECT * FROM relaxation_activities";
-      $result = $conn->query($sql);
-
-      if (!$result) {
-        $errorInfo = $conn->errorInfo();
-        die('Error executing query: ' . $errorInfo[2]);
-      }
-
-        echo '
-        <div class="content">';
-
-      if($result->rowCount() > 0) {
-        
-        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-          $name = htmlspecialchars( $row['activity_name']);
-          $description = htmlspecialchars($row['description']);
-          $file_name = htmlspecialchars($row['image_url']);
-
-          echo '
-          <div class="container">
-            <div class="card-div">
-              <div class="card">
-                <div class="image-content">
-                  <span class="overlay"></span>
-                  <div class="card-image">
-                    <img src="./uploads/'.$file_name.'" alt="'.$name.'" class="card-img">
-                  </div>
-                </div>
-                <div class="card-content">
-                  <h2 class="activity">'.$name.'</h2>
-                  <p class="description">
-                    '.$description.'
-                  </p>
-                  <button class="button">View More</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        ';
-      }
-        
-      } else {
-        echo "<p>No relaxation activities found.</p>";
-      }
-    
-
-      echo '
-          </div>
-        </div>';
-        }
+    public function __construct() {
+        $this->model = new ViewRelaxationActivityModel();
     }
+
+    public function handleRequest() {
+        $activities = $this->model->getAllActivities(); // Fetch all activities
+
+        echo '<div class="content">';
+
+        if (!empty($activities)) {
+            foreach ($activities as $row) {
+                $name = htmlspecialchars($row['activity_name']);
+                $description = htmlspecialchars($row['description']);
+                $file_name = htmlspecialchars($row['image_url']);
+
+                echo '
+                <div class="container">
+                    <div class="card-div">
+                        <div class="card">
+                            <div class="image-content">
+                                <span class="overlay"></span>
+                                <div class="card-image">
+                                    <img src="./uploads/' . $file_name . '" alt="' . $name . '" class="card-img">
+                                </div>
+                            </div>
+                            <div class="card-content">
+                                <h2 class="activity">' . $name . '</h2>
+                                <p class="description">' . $description . '</p>
+                                <button class="button">View More</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>';
+            }
+        } else {
+            echo "<p>No relaxation activities found.</p>";
+        }
+
+        echo '</div>';
+    }
+}
+
 ?>
