80163bfc4e84ea472e5985b9c8a555c4c16881af
ovindu <ovindumandith@gmail.com>
2025-04-28 11:45:29 +0530

    css changes student home

--- App/views/home.php
+++ App/views/home.php
@@ -51,7 +51,11 @@ if ($latestStressLevel === 'Low') {
     />
     <link rel="stylesheet" href="../../assets/css/home.css" type="text/css" />
     <link rel="stylesheet" href="../../assets/css/dashboard.css" type="text/css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+
+
+
 
     <script src="../../assets/js/hero_slider.js" defer></script>
     <script src="../../assets/js/testimonial_slider.js" defer></script>
